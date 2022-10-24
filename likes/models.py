8b7fe108c9bd7ef590c1bc1087f5fd4a9025ed26
Motoki Higa <mokkyhiga@gmail.com>
2022-10-24 16:12:19 +0900

    feat: add generic relationship to tags

--- likes/models.py
+++ likes/models.py
@@ -1,15 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-
-class TaggedItem(models.Model):
-    # what tag applied to what object
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # To define a Generic Relationship, there are three fields you need to define
     # content type, object id and content object 
